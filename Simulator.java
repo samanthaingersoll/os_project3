812cd8f56c8587cbc4616430ee94f264a5528e09
Tedd Oravec <mail.teddoravec@googlemail.com>
2019-04-23 16:23:12 -0500

    Added look for txt in args

--- Simulator.java
+++ Simulator.java
@@ -1,6 +1,18 @@
 public class Simulator { 
     public static void main(String[] args) { 
-    
         
+
+        for (int i = 0; i < args.length; i++) {
+            if (args[i].contains(".txt")) {
+                //TODO: Import txt to array
+                try (Stream<String> stream = Files.lines(Paths.get(fileName))) {
+                    stream.forEach(System.out::println);
+                }
+            } else {
+                //TODO: call RNG to create next-track array
+            }
+        }
+        
+
     }
 }
